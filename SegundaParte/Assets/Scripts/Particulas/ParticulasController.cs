7a7c89be4607897099d7035f044c3b1e2565a3da
Usuario-PC\Des. de videojuegos <jorongo94@gmail.com>
2017-12-18 18:53:16 +0100

    Dia 18 (1)

---
+++ SegundaParte/Assets/Scripts/Particulas/ParticulasController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticulasController : MonoBehaviour {
+
+    public ParticleSystem particulas;
+	
+	void Start () {
+        particulas = GetComponent<ParticleSystem>();
+        //para poder cambiar algun valor de un modulo de las particulas...
+
+        ParticleSystem.EmissionModule emision = particulas.emission;
+        emision.rateOverTime = 10f;
+	}
+	
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            particulas.Play();
+        } else if (Input.GetKeyDown(KeyCode.B))
+        {
+            particulas.Stop();
+            
+            
+        }
+	}
+}
